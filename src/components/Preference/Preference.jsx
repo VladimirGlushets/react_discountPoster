abe8e5f96f9707a8439942f16928b2e1d0038701
uladzimir.hlushets <uladzimir.hlushets@BY-IT00199>
2022-12-13 20:30:36 +0300

    pref css

--- src/components/Preference/Preference.jsx
+++ src/components/Preference/Preference.jsx
@@ -2,17 +2,23 @@ import React from "react";
 import Button from "../Button/Button";
 import "./Preference.css";
 
-
-function Preference({preference, onDelete}) {
-    let desc = "" + preference.categoryId;
+function Preference({ preference, onDelete }) {
+  let desc = "" + preference.categoryId;
   return (
     <>
-      <div className="filter"><p>{desc}</p>      
+      <div className="filter">
+        <p>{desc}</p>
         <Button
-          title={"Delete"}
+          title={"Edit"}
+          onClick={() => onDelete(preference)}
+          className={"filter_edit_button"}
+        />
+        <Button
+          title={"X"}
           onClick={() => onDelete(preference)}
           className={"filter_delete_button"}
-        /></div>
+        />        
+      </div>
     </>
   );
 }
